31b5c12b97dfc8327ab8925a0af2bd35dd693b5a
Marek Bareła <marek.barela@wp.pl>
2019-05-09 00:34:20 +0200

    components structure update

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -2,7 +2,8 @@ import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
+import Header from "../header/header";
+import Footer from '../footer/footer';
 import "./layout.css"
 
 const Layout = ({ children }) => (
@@ -19,19 +20,10 @@ const Layout = ({ children }) => (
     render={data => (
       <>
         <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}
-          </footer>
-        </div>
+        <main>
+          {children}
+        </main>
+        <Footer />
       </>
     )}
   />
