f69ed8af2c35d03b55ec5cb0ecec59bc59c41daf
Marek Bareła <marek.barela@wp.pl>
2019-05-10 00:07:04 +0200

    dynamic counter feature commited

--- src/components/statistics-element/element.js
+++ src/components/statistics-element/element.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Favicon from '../favicon/favicon';
+import CountUp from 'react-countup';
 import styles from './element.module.css';
 import PropTypes from 'prop-types';
 
@@ -10,7 +11,7 @@ const StatisticsElement = (props) => {
     <div className={statElement}>
       <Favicon icon={icon} />
       <div className={statTextContainer}>
-        <span className={counter}>{number}</span>
+        <span className={counter}><CountUp start={0} end={number} duration={10} /></span>
         <span className={label}>{labelText}</span>
       </div>
     </div>
