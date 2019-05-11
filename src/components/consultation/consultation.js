d7bf6fc5c4b1b2e0ac55a5502ebc534016cca1b5
Marek Bareła <marek.barela@wp.pl>
2019-05-11 19:49:39 +0200

    form styling commited

--- src/components/consultation/consultation.js
+++ src/components/consultation/consultation.js
@@ -3,26 +3,28 @@ import SectionHeader from '../section-header-left/header';
 import styles from './consultation.module.css';
 
 const Consultation = () => {
-  const { wrapper, formContainer, imageContainer } = styles;
+  const { wrapper, formContainer, imageContainer, form, inputWrapper, textarea } = styles;
   return (
     <section id="consultation">
       <div className={wrapper}>
         <div className={formContainer}>
           <SectionHeader
             header="Free Consultation"
-            paragraph="tLorem ipsum dolor amet, consectetur adipisice elite sede eiusmod tempor incidide labeore dolore magna."
+            paragraph="Lorem ipsum dolor amet, consectetur adipisice elite sede eiusmod tempor incidide labeore dolore magna."
           />
-          <form onSubmit={console.log("Message has been send")}>
-            <input type="name" placeholder="Your Name" />
-            <input type="email" placeholder="Your Email" />
-            <select name="" id="">
+          <form onSubmit={console.log("Message has been send")} className={form}>
+            <div className={inputWrapper}>
+              <input type="name" placeholder="Your Name" />
+              <input type="email" placeholder="Your Email" />
+            </div>
+            <select name="">
               <option value="1">Practive Area</option>
-              <option value="1">Criminal Law</option>
-              <option value="1">International Law</option>
-              <option value="1">Financial Law</option>
+              <option value="2">Criminal Law</option>
+              <option value="3">International Law</option>
+              <option value="4">Financial Law</option>
             </select>
-            <textarea placeholder="Your Message" />
-            <button type="submit">send</button>
+            <textarea placeholder="Your Message" className={textarea} />
+            <button type="submit">send request</button>
           </form>
         </div>
         <div className={imageContainer}></div>
