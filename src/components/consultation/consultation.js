c77eb0a95c8e3cb25410a7e0511b470cb21e55ce
Marek Bareła <marek.barela@wp.pl>
2019-05-11 19:06:25 +0200

    basic form commited

--- src/components/consultation/consultation.js
+++ src/components/consultation/consultation.js
@@ -12,10 +12,20 @@ const Consultation = () => {
             header="Free Consultation"
             paragraph="tLorem ipsum dolor amet, consectetur adipisice elite sede eiusmod tempor incidide labeore dolore magna."
           />
+          <form onSubmit={console.log("Message has been send")}>
+            <input type="name" placeholder="Your Name" />
+            <input type="email" placeholder="Your Email" />
+            <select name="" id="">
+              <option value="1">Practive Area</option>
+              <option value="1">Criminal Law</option>
+              <option value="1">International Law</option>
+              <option value="1">Financial Law</option>
+            </select>
+            <textarea placeholder="Your Message" />
+            <button type="submit">send</button>
+          </form>
         </div>
-        <div className={imageContainer}>
-
-        </div>
+        <div className={imageContainer}></div>
       </div>
     </section>
   )
