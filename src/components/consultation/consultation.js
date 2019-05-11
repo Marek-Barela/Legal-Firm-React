b02da89bbe50d03d043450329b3c622865b515c8
Marek Bareła <marek.barela@wp.pl>
2019-05-11 04:49:48 +0200

    headerexported to separete component and used in contact section

--- src/components/consultation/consultation.js
+++ src/components/consultation/consultation.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React from 'react';
+import SectionHeader from '../section-header-left/header';
+import styles from './consultation.module.css';
 
 const Consultation = () => {
+  const { wrapper, formContainer } = styles;
   return (
     <section>
-      <div>
-
+      <div className={wrapper}>
+        <div className={formContainer}>
+          <SectionHeader
+            header="Free Consultation"
+            paragraph="tLorem ipsum dolor amet, consectetur adipisice elite sede eiusmod tempor incidide labeore dolore magna."
+          />
+        </div>
       </div>
     </section>
   )
