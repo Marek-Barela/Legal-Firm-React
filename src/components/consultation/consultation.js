b62575e567bac8e2c3c10838599fc8bcddd4d5cb
Marek Bareła <marek.barela@wp.pl>
2019-05-11 04:54:13 +0200

    contact section: image container commited

--- src/components/consultation/consultation.js
+++ src/components/consultation/consultation.js
@@ -3,15 +3,18 @@ import SectionHeader from '../section-header-left/header';
 import styles from './consultation.module.css';
 
 const Consultation = () => {
-  const { wrapper, formContainer } = styles;
+  const { wrapper, formContainer, imageContainer } = styles;
   return (
-    <section>
+    <section id="consultation">
       <div className={wrapper}>
         <div className={formContainer}>
           <SectionHeader
             header="Free Consultation"
             paragraph="tLorem ipsum dolor amet, consectetur adipisice elite sede eiusmod tempor incidide labeore dolore magna."
           />
+        </div>
+        <div className={imageContainer}>
+
         </div>
       </div>
     </section>
